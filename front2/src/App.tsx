c033143cd791b6c9f98ec056d828c223e81a0498
Herick José <hjf@academico.ufpb.br>
2025-03-24 17:41:25 -0300

    Mais ajustes no front (home)

---
+++ front2/src/App.tsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import NotFound from "./pages/notFound/NotFound";
+import Nav from "./pages/nav/nav";
+import './css/initialpages/App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+
+      <Nav />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    
+
+    </div>
+  );
+};
+
+export default App
