64280fa2a94a9fea6a9fbed477099feb2e3b250d
rafaelfranca1 <rafaeldefranca750@gmail.com>
2025-03-26 23:55:57 -0300

    feat: add API option for selecting music api on service in search and lyrics fetch
    Co-authored-by: Luis Reis <LuisReis09@users.noreply.github.com>

--- back/src/api-requests/api-requests.controller.ts
+++ back/src/api-requests/api-requests.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApiRequestsService } from './api-requests.service';
+import { ApiQuery } from '@nestjs/swagger';
 
 @Controller('api-requests')
 export class ApiRequestsController {
   constructor(private readonly apiRequestsService: ApiRequestsService) {}
 
   @Get('search')
+  @ApiQuery({
+    name: 'api_option',
+    description:
+      'Escolha a API para buscar as letras: 1 para Spotify, 2 para Musixmatch, 3 para Vagalume',
+    required: true,
+    enum: ['1', '2', '3'], // Define as opções disponíveis
+  })
   async searchTracks(
     @Query('track') track: string,
     @Query('artist') artist: string,
@@ -15,6 +23,13 @@ export class ApiRequestsController {
   }
 
   @Get('lyrics')
+  @ApiQuery({
+    name: 'api_option',
+    description:
+      'Escolha a API para buscar as letras: 1 para Letras.mus, 2 para Musixmatch, 3 para Vagalume',
+    required: true,
+    enum: ['1', '2', '3'], // Define as opções disponíveis
+  })
   async getLyrics(
     @Query('track') track: string,
     @Query('artist') artist: string,
@@ -22,5 +37,4 @@ export class ApiRequestsController {
   ): Promise<any> {
     return this.apiRequestsService.getLyrics(track, artist, api_option);
   }
-
 }
--- back/src/api-requests/api-requests.service.ts
+++ back/src/api-requests/api-requests.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import * as dotenv from 'dotenv';
 import * as cheerio from 'cheerio';
-import puppeteer from 'puppeteer';
 
 dotenv.config();
 
@@ -40,7 +39,10 @@ export class ApiRequestsService {
     }
   }
 
-  private async searchTracks_Spotify(track: string, artist: string): Promise<any> {
+  private async searchTracks_Spotify(
+    track: string,
+    artist: string,
+  ): Promise<any> {
     const accessToken = await this.getAccessToken();
     const query = `track:${track} artist:${artist}`;
     const numberOfResults = 5;
@@ -70,11 +72,14 @@ export class ApiRequestsService {
     return tracks;
   }
 
-  private async searchTracks_musixmatch(track: string, artist: string): Promise<any> {
+  private async searchTracks_musixmatch(
+    track: string,
+    artist: string,
+  ): Promise<any> {
     let limit = 5;
 
     const result = await axios.get(
-      `https://api.musixmatch.com/ws/1.1/track.search?q_track=${track}&q_artist=${artist}&apikey=${this.musixmatchApiKey}&f_has_lyrics=1&page_size=${limit}`
+      `https://api.musixmatch.com/ws/1.1/track.search?q_track=${track}&q_artist=${artist}&apikey=${this.musixmatchApiKey}&f_has_lyrics=1&page_size=${limit}`,
     );
 
     const data: any = result.data;
@@ -82,7 +87,7 @@ export class ApiRequestsService {
     const tracks = data.message.body.track_list.map((item: any) => ({
       track_name: item.track.track_name,
       artist_name: item.track.artist_name,
-      album_image: item.track.album_coverart_100x100
+      album_image: item.track.album_coverart_100x100,
     }));
 
     return tracks;
@@ -90,20 +95,20 @@ export class ApiRequestsService {
 
   async searchTracks_Vagalume(track: string, artist: string): Promise<any> {
     let limit = 5;
+    const query = `${artist} ${track}`;
 
     const result = await axios.get(
-      `https://api.vagalume.com.br/search.php?art=${artist}&mus=${track}&apikey=${this.vagalumeApiKey}`
+      `https://api.vagalume.com.br/search.excerpt?q=${query}&apikey=${this.vagalumeApiKey}&limit=${limit}`,
     );
 
     const data: any = result.data;
-    const tracks = data.mus.map((item: any) => ({
-      track_name: item.name,
-      artist_name: item.band,
-      album_image: item.pic_small
-    }));
+    // const tracks = data.mus.map((item: any) => ({
+    //   track_name: item.name,
+    //   artist_name: item.band,
+    //   album_image: item.pic_small,
+    // }));
 
-    return tracks;
-    
+    return data;
   }
 
   async getAudioPreview(track: string, artist: string): Promise<string | null> {
@@ -130,73 +135,107 @@ export class ApiRequestsService {
     return data.tracks.items[0].preview_url;
   }
 
-  async searchTracks(track: string, artist: string, api_option: string|number): Promise<any> {
-    if(api_option == '1'){
-      return this.searchTracks_Spotify(track, artist);
-    } else if(api_option == '2'){
-      let obj = await this.searchTracks_musixmatch(track, artist);
-      obj["audio_preview"] = await this.getAudioPreview(track, artist);
-      return obj;
-    } else if(api_option == '3'){
-      return this.searchTracks_Vagalume(track, artist);
+  async searchTracks(
+    track: string,
+    artist: string,
+    api_option: string | number,
+  ): Promise<any> {
+    switch (api_option) {
+      case '1':
+        return this.searchTracks_Spotify(track, artist);
+      case '2': {
+        const obj = await this.searchTracks_musixmatch(track, artist);
+        obj['audio_preview'] = await this.getAudioPreview(track, artist);
+        return obj;
+      }
+      case '3': {
+        return await this.searchTracks_Vagalume(track, artist);
+        // obj['audio_preview'] = await this.getAudioPreview(track, artist);
+        // return obj;
+      }
+      default:
+        return { error_code: 400, message: 'Invalid API option' };
     }
-
-    return {error_code: 400, message: "Invalid API option"};
   }
 
+  private async getLyrics_letrasmus(
+    songName: string,
+    artistName: string,
+  ): Promise<string> {
+    // Formatação do nome da música e do artista para se ajustar à URL do site
+    const formattedSong = songName.replace(/ /g, '-').toLowerCase();
+    const formattedArtist = artistName.replace(/ /g, '-').toLowerCase();
 
-  private async getLyrics_letrasmus(track: string, artist: string): Promise<string | undefined> {
-    
-    // Primeiro, formata os parâmetros para a URL
-    track = track.toLowerCase().replace(/ /g, '-');
-    artist = artist.toLowerCase().replace(/ /g, '-');
-    const url = `https://www.letras.mus.br/${artist}/${track}/`;
+    const url = `https://www.letras.mus.br/${formattedArtist}/${formattedSong}/`;
 
     try {
-      // Faz a requisição para a página da letra
       const response = await axios.get(url);
-      const $ = cheerio.load(response.data);
 
-      // Extrai a letra da música
-      const lyrics = $('.lyric-original div').text();
-      return lyrics;
+      if (response.status === 200) {
+        const $ = cheerio.load(response.data);
+        const lyricsDiv = $('.lyric-original');
+
+        if (lyricsDiv.length > 0) {
+          return lyricsDiv.text().trim().replace(/\n+/g, '\n');
+        } else {
+          return 'Letra não encontrada na página.';
+        }
+      } else {
+        return 'Erro ao acessar a página da música.';
+      }
     } catch (error) {
-      console.error('Erro ao buscar a letra da música:', error);
-      return undefined;
+      return `Erro ao buscar a letra: ${error}`;
     }
-
   }
 
   // Substituir chamada da api por chamada direta ao site, usando scraping
-  private async getLyrics_musixmatch(track: string, artist: string): Promise<string | undefined> {
-    track = track.replace(/ /g, '-');
-    artist = artist.replace(/ /g, '-');
-    const url = `https://www.musixmatch.com/lyrics/${artist}/${track}`;
+  private async getLyrics_musixmatch(
+    songName: string,
+    artistName: string,
+  ): Promise<string> {
+    // Formatação do nome da música e do artista para se ajustar à URL do site
+    songName = songName.replace(' ', '-');
+    artistName = artistName.replace(' ', '-');
+
+    const headers = {
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    };
+
+    // Construção da URL da página da música no Musixmatch
+    const url = `https://www.musixmatch.com/lyrics/${artistName}/${songName}`;
 
     try {
-      const browser = await puppeteer.launch({ headless: "new" });
-      const page = await browser.newPage();
-      await page.goto(url, { waitUntil: 'networkidle2' });
+      // Realizando a requisição HTTP
+      const response = await axios.get(url, { headers });
 
-      // Aguarda o seletor correto carregar
-      await page.waitForSelector('.css-175oi2r .r-18u37iz .r-1w6e6rj');
+      // Verificando se a requisição foi bem-sucedida (status 200)
+      if (response.status === 200) {
+        const $ = cheerio.load(response.data);
 
-      // Captura a letra da música
-      const lyrics = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('.css-175oi2r .r-18u37iz .r-1w6e6rj'))
-          .map(el => el.textContent?.trim())
-          .join('\n');
-      });
-
-      await browser.close();
-      return lyrics || undefined;
+        // A letra geralmente está dentro de divs com as classes específicas
+        const lyricsSpans = $('.css-175oi2r.r-18u37iz.r-1w6e6rj');
+        if (lyricsSpans.length > 0) {
+          const lyrics = lyricsSpans
+            .map((index, span) => $(span).text().trim())
+            .get()
+            .join('\n');
+          return lyrics;
+        } else {
+          return 'Letra não encontrada na página.';
+        }
+      } else {
+        return 'Erro ao acessar a página da música.';
+      }
     } catch (error) {
-      console.error('Erro ao buscar a letra no Musixmatch:', error.message);
-      return undefined;
+      return `Erro ao buscar a letra: ${error.message}`;
     }
   }
 
-  private async getLyrics_vagalume(track: string, artist: string): Promise<string | null> {
+  private async getLyrics_vagalume(
+    track: string,
+    artist: string,
+  ): Promise<string | null> {
     track = track.toLowerCase().replace(/ /g, '-');
     artist = artist.toLowerCase().replace(/ /g, '-');
     const url = `https://www.vagalume.com.br/${artist}/${track}.html`;
@@ -205,25 +244,31 @@ export class ApiRequestsService {
       const response = await axios.get(url);
       const $ = cheerio.load(response.data);
 
-      const lyrics = $('#lyrics').text();
-      return lyrics;
+      // Seleciona o elemento com id "lyrics" e processa os <br> como quebras de linha
+      const lyrics = $('#lyrics')
+        .contents()
+        .map((_, el) => ($(el).is('br') ? '\n' : $(el).text()))
+        .get()
+        .join('');
+
+      return lyrics.trim(); // Remove espaços extras no início e no final
     } catch (error) {
       console.error('Erro ao buscar a letra da música:', error);
       return null;
     }
-
   }
 
-  async getLyrics(track: string, artist: string, api_option:string|number){
-    if(api_option == '1'){
-      return this.getLyrics_letrasmus(track, artist);
-    } else if(api_option == '2'){
-      return this.getLyrics_musixmatch(track, artist);
-    } else if(api_option == '3'){
-      return this.getLyrics_vagalume(track, artist);
+  async getLyrics(track: string, artist: string, api_option: string | number) {
+    switch (api_option) {
+      case '1':
+        return this.getLyrics_letrasmus(track, artist);
+      case '2':
+        return this.getLyrics_musixmatch(track, artist);
+      case '3':
+        return this.getLyrics_vagalume(track, artist);
+      default:
+        return { error_code: 400, message: 'Invalid API option' };
     }
-
-    return {error_code: 400, message: "Invalid API option"};
   }
 }
 
