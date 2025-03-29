088a940722d001b3e22c5ea9af31f2b40b49637a
Herick José <hjf@academico.ufpb.br>
2025-03-29 01:17:12 -0300

    Co-authored-by: Luis Reis <LuisReis09@users.noreply.github.com>

---
+++ front/src/pages/home/Avatars.tsx
@@ -0,0 +1,13 @@
+
+
+const Avatars: React.FC = () => {
+    return (
+        <div className="avatars_selection_box">
+            {
+                
+            }
+        </div>
+    );
+}
+
+export default Avatars;
