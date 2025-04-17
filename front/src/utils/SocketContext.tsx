56df9119a78d54ee7b4e4d9259957c20ba3eb180
LuisReis09 <luis.reis@academico.ufpb.br>
2025-04-17 20:21:14 -0300

    refactor: clean up .dockerignore files and adjust CSS for responsive design in players and requests sections

--- front/src/utils/SocketContext.tsx
+++ front/src/utils/SocketContext.tsx
@@ -10,7 +10,6 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
 
 export function SocketProvider({ children }: { children: React.ReactNode }) {
   // Criando a conexão com o servidor (MANTENDO UMA ÚNICA INSTÂNCIA
-  console.log(BACKEND_URL)
   const socket = useMemo(() => io(`${BACKEND_URL}`), []);
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
